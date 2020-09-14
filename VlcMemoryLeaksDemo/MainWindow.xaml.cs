e2937a09b6d6ddc03a4508b8147bad9d8f2f9523
Petr Drábek <petr.drabek@ki-wi.cz>
2020-09-14 14:39:17 +0200

    Initialize commit

---
+++ VlcMemoryLeaksDemo/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VlcMemoryLeaksDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VlcViewer _vlcViewer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            var timer = new Timer(2000);
+            timer.AutoReset = true;
+            timer.Elapsed += PlayVlcVideo;
+            timer.Start();
+        }
+
+
+        private void PlayVlcVideo(object sender, ElapsedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (_vlcViewer != null)
+                {
+                    _vlcViewer.Stop();
+                    _vlcViewer.Dispose();
+                }
+
+                _vlcViewer = new VlcViewer();
+                ParentGrid.Children.Add(_vlcViewer.Display);
+                _vlcViewer.Play();
+            });
+        }
+    }
+}
---
+++ VlcMemoryLeaksDemo/VlcViewer.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
+using System.Windows.Forms.Integration;
+
+namespace VlcMemoryLeaksDemo
+{
+    class VlcViewer
+    {
+        #region Fields
+
+        private readonly WindowsFormsHost _formHost;
+
+        private LibVLC _libVlc;
+
+        private LibVLCSharp.Shared.MediaPlayer _mediaPlayer;
+
+        private VideoView _videoView;
+
+        private static bool _libVlcCoreLoaded;
+
+        #endregion
+
+        #region Properties and Indexers
+
+        public UIElement Display => _formHost;
+
+        #endregion
+
+        #region Constructors
+
+        public VlcViewer()
+        {
+            InitializeVlc();
+            SetMedium();
+            _formHost = new WindowsFormsHost {Child = _videoView};
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Closes this instance.
+        /// </summary>
+        public void Dispose()
+        {
+            _videoView?.Dispose();
+            if (_mediaPlayer != null)
+            {
+                _mediaPlayer.EndReached -= VlcOnEndReached;
+                _mediaPlayer.EncounteredError -= OnEncounteredError;
+                _mediaPlayer.Dispose();
+            }
+            _libVlc?.Dispose();
+
+            if (_formHost == null)
+                return;
+            _formHost.Child = null;
+            _formHost.Dispose();
+        }
+
+        public void Play()
+        {
+            _mediaPlayer.EncounteredError += OnEncounteredError;
+            _mediaPlayer.EndReached += VlcOnEndReached;
+
+            if (!_mediaPlayer.Play())
+                OnMediaFailed();
+        }
+
+        public void SetMedium()
+        {
+            _mediaPlayer.Media = new Media(_libVlc, new Uri(@"file:///C:/kiwi-platform/Content/cdn-us.ki-wi.cz/443/f/fit/Euronics - black friday Samsung.mp4"));
+        }
+
+        /// <summary>
+        /// Stops this instance.
+        /// </summary>
+        public void Stop()
+        {
+            if (_mediaPlayer.IsPlaying)
+            {
+                _mediaPlayer.Pause();
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void OnEncounteredError(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(_ => OnMediaFailed());
+        }
+
+        private void VlcOnEndReached(object sender, EventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(_ => OnMediaEnded());
+        }
+
+        /// <summary>
+        /// Inicializuje VLC
+        /// </summary>
+        private void InitializeVlc()
+        {
+            if (!_libVlcCoreLoaded)
+            {
+                // Načtení nativní LibVlc - stačí jednou po celý běh aplikace
+                Core.Initialize();
+                _libVlcCoreLoaded = true;
+            }
+            _libVlc = new LibVLC();
+            _mediaPlayer = new LibVLCSharp.Shared.MediaPlayer(_libVlc);
+            _videoView = new VideoView { MediaPlayer = _mediaPlayer };
+        }
+
+        /// <summary>
+        /// Called when [media failed].
+        /// </summary>
+        protected virtual void OnMediaFailed()
+        {
+            // Do some stuff
+        }
+
+        /// <summary>
+        /// Called when [media ended].
+        /// </summary>
+        protected virtual void OnMediaEnded()
+        {
+            // Do some stuff
+        }
+
+        #endregion
+    }
+}
